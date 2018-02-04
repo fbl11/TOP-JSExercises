2ce27d0e6a6c78461cc624a6b4528f1e2c39c548
freya <freyab_london@yahoo.co.uk>
2018-02-04 13:42:10 +0000

    fb11 solutions - HW, Palindr, RemFmArr, RepeatStr, ReverseStr, snakeCase, fib

--- fibonacci/README.md
+++ fibonacci/README.md
@@ -8,3 +8,18 @@ Create a function that returns a specific member of the fibonacci sequence:
 fibonacci(4) // returns the 4th member of the series: 3  (1, 1, 2, 3)
 fibonacci(6) // returns 8
 ```
+
+
+create array containing fibonacci numbers up to position of 'specific number'
+	add current number to previous number
+	if no previous number add to itself
+	add new number to the end of the array
+
+return specific number
+	array lookup +1
+
+negative number input returns "oops"
+	check if number negative, if yes, return oops, if no follow above
+
+accept strings
+	take string and convert to number
--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,34 @@
-var fibonacci = function() {
+// alternative one: Creates all fibonacci numbers between 1 and num, and retains full array
+// var fibonacci = function(num) {
+  // var fibNum = [1, 1];
+  // i = 2;
 
-}
+  // if (num < 0) {
+    // return "OOPS";
+  // } else {
+    // while (i < num) {
+    // fibNum.push(fibNum[fibNum.length - 1] + fibNum[fibNum.length - 2]);
+    // i++;
+    // }
+  // }
+  // return(fibNum[fibNum.length - 1]);
+// };
+
+//alternative two: never retains more than three elements within array - current element, previous element, current element + previous element
+var fibonacci = function(num) {
+  var fibNum = [1, 1];
+  i = 2;
+
+  if (num < 0) {
+    return "OOPS";
+  } else {
+    while (i < num) {
+    fibNum.unshift(fibNum[0] + fibNum[1]);
+    fibNum.pop();
+    i++;
+    }
+  }
+  return(fibNum[0]);
+};
 
 module.exports = fibonacci
