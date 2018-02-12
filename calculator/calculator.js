5dcd60593fb744a7631d2dbedccd906298598836
freya <freyab_london@yahoo.co.uk>
2018-02-12 21:32:07 +0000

    fbl11 solutions - all except generator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((prev, curr) => prev + curr, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((prev, curr) => prev * curr, 1);
 }
 
-function power() {
-	
+function power (num1, num2) {
+	// return Math.pow(num1, num2);
+  var num = num1;
+  i = 1;
+
+  while (i < num2) {
+    num *= num1;
+    i++;
+  }
+  return num;
 }
 
-function factorial() {
-	
+
+function factorial(num) {
+	var result = 1;
+	i = 1;
+
+	while (i <= num) {
+		result *= i;
+		i++;
+	}
+	return result;
 }
 
 module.exports = {
